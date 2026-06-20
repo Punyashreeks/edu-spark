967e99b8729a5ee3592dc22b8ffec198d6ef2e7c
unknown <Punyashreeks>
2026-06-21 00:02:06 +0530

    Added subject view component

---
+++ components/subject-view.tsx
@@ -0,0 +1,13 @@
+export default function SubjectView() {
+  return (
+    <div>
+      <h2>Subjects</h2>
+      <ul>
+        <li>Mathematics</li>
+        <li>Physics</li>
+        <li>Biology</li>
+        <li>Earth Science</li>
+      </ul>
+    </div>
+  );
+}

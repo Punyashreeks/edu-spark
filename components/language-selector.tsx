988b1d51fbd95fee1da5b0c5b6b148f982e92caa
unknown <Punyashreeks>
2026-06-21 00:12:08 +0530

    Added welcome header component

---
+++ components/language-selector.tsx
@@ -0,0 +1,10 @@
+export default function LanguageSelector() {
+  return (
+    <select>
+      <option>English</option>
+      <option>Kannada</option>
+      <option>Hindi</option>
+      <option>Telugu</option>
+    </select>
+  );
+}

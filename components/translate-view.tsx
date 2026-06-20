a5da793f8af9ca0ca2b7b50b1e08a148b587fb15
unknown <Punyashreeks>
2026-06-21 00:09:03 +0530

    Added user profile component

---
+++ components/translate-view.tsx
@@ -0,0 +1,8 @@
+export default function TranslateView() {
+  return (
+    <div>
+      <h2>Translate Content</h2>
+      <button>Translate</button>
+    </div>
+  );
+}
---
+++ components/user-profile.tsx
@@ -0,0 +1,9 @@
+export default function UserProfile() {
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: Student</p>
+      <p>Level: Beginner</p>
+    </div>
+  );
+}
